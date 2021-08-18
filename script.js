8bd73f4d7274037d3efca347762c432d58a881db
juju-v21 <85849575+juju-v21@users.noreply.github.com>
2021-08-18 10:58:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+window.addEventListener("scroll", function()
+{
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+})
+
+
+const navigation = document.querySelector('nav');
+
+document.querySelector('.toggle').onclick = 
+function(){
+    this.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+// gallerie //
+
+document.querySelectorAll('.image-container img').forEach(image =>{ 
+    image.onclick = () =>{
+        document.querySelector('.popup-image').style.display ='block';
+        document.querySelector('.popup-image img').src = image.getAttribute('src');
+        }
+    });
+
+    document.querySelector('.popup-image span').onclick = () =>{
+        document.querySelector('.popup-image').style.display = 'none';
+    };
